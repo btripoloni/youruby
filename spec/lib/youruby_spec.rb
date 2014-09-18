2d50833dd8a39d1f26044a71b7b55f3fb7e59e74
vagrant <vagrant@rails-dev-box>
2014-09-18 22:28:18 +0000

    add tests

--- spec/lib/youruby_spec.rb
+++ spec/lib/youruby_spec.rb
@@ -1,19 +1,47 @@
 require "spec_helper"
+require "fakefs/spec_helpers"
 require "youruby"
 
 describe Youruby do
-  it "get video name" do
-    video = Youruby::Youtube.new('uaEJvYWc2ag')
-    video.id.should == "uaEJvYWc2ag"
+  include FakeFS::SpecHelpers
+  let(:video) {Youruby::Youtube.new('uaEJvYWc2ag')}
+  context 'Youtube'do
+    describe 'get' do
+      it ".video_title" do
+        expect(video.get_title).to eq("FFmpeg-slowmotion.1\n")
+      end
+
+      it ".video_id" do
+        expect(video.get_id).to eq("uaEJvYWc2ag")
+      end
+
+      it ".video_url" do
+
+        expect(video.get_url).to eq("http://youtube.com/watch?v=uaEJvYWc2ag")
+      end
+
+      it ".get_video" do
+        FakeFS do
+          video.get_video
+          File.exists? "#{video.get_title}-#{video.get_id}.mp4"
+        end
+      end
+
+      it '.get_thumbnails' do
+        expect(video.get_thumbnails['quality']['default']).to eq("http://img.youtube.com/vi/uaEJvYWc2ag/default.jpg")
+      end
+    end
+
+    describe 'set' do
+      it '.set_url' do
+        video.set_url("https://www.youtube.com/watch?v=lIzAlULvipk")
+        expect(video.get_id).to eq("lIzAlULvipk")
+      end
+
+      it 'set_path' do
+        pending
+      end
+    end
   end
-  it "get video id" do
-    video = Youruby::Youtube.new('uaEJvYWc2ag')
-    video.id.should == "uaEJvYWc2ag"
-  end
-  it "get video url" do
-    video = Youruby::Youtube.new('uaEJvYWc2ag')
-    url = video.get_url
-    url.should == "http://youtube.com/watch?v=uaEJvYWc2ag"
-  end
-  it "get video"
+
 end
