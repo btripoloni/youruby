80e09612d9af050fc3e77e6530d5989225275733
btripoloni <bruno.tripoloni@gmail.com>
2014-09-20 03:50:17 +0000

    better syntax

--- lib/youruby.rb
+++ lib/youruby.rb
@@ -1,36 +1,53 @@
-require "youruby/version"
-require "uri"
-require "cgi"
+require 'youruby/version'
+require 'uri'
+require 'cgi'
 
+# the Youruby is a base of this gem
+#
 module Youruby
+  # Youtube Class
+  #
+  # @example
+  #   Youruby::Youtube.new(id)
+  #
+  # @param id [String] the id of a video from Youtube
+  #
   class Youtube
-    attr_reader :id
-
-    YT_DL = File.join(File.expand_path(File.dirname(__FILE__)), "../bin/youtube-dl")
+    attr_reader :id, :location
+    root_path = File.expand_path(File.dirname(__FILE__))
+    yt_path = '../bin/youtube-dl'
+    YT_DL = File.join(root_path, yt_path)
 
     def initialize(id)
       @id = id
-      @location = "temp/download"
-      @thumbnails
+      @location = 'temp/download'
     end
 
     def title
-      title = `#{YT_DL} --get-title #{get_url}`
+      title = `#{YT_DL} --get-title #{url}`
       title.delete("\n")
     end
 
-    def url
-      "http://youtube.com/watch?v=#{@id}"
-    end
-
     def file_path
-      File.join(@location, "#{get_title}-#{get_id}.mp4")
+      File.join(@location, "#{title}-#{id}.mp4")
     end
 
-    def url(url)
-      url = URI.parse url
-      url_parse = params(url.query)
-      id(url_parse['v'].first)
+    def url(url = nil)
+      if url
+        url = URI.parse url
+
+        url_parse = params(url.query)
+
+        @id = url_parse['v'].first
+
+        true
+      else
+        "http://youtube.com/watch?v=#{@id}"
+      end
+    end
+
+    def thumbnail(path)
+      "http://img.youtube.com/vi/#{@id}/#{path}.jpg"
     end
 
     def path(path)
@@ -39,58 +56,13 @@ module Youruby
     end
 
     def download_video
-      system(YT_DL, '-o', file_path, get_url, '--no-progress')
+      system(YT_DL, '-o', file_path, url, '--no-progress')
     end
 
     private
 
-    def id(id)
-      @id = id
-      true
-    end
-
     def params(body)
       CGI.parse(body)
-    end
-
-    def thumbnail_url(path)
-      "http://img.youtube.com/vi/#{@id}/#{path}.jpg"
-    end
-
-    def thumbnails
-      thumbnails = Hash.new
-
-      frame = 0
-      frames = Hash.new
-      while frame < 3
-        frames < {frame => thumbnail_url(frame)}
-      end
-
-      quality = ['', 'hq', 'mq', 'sd', 'maxres']
-      qualities = Hash.new
-      count = 0
-      while count < quality.lenght
-        description = "#{quality[count]}default"
-        qualities < {description => thumbnail_url(description)}
-      end
-
-      thumbnails < frames < qualities
-
-      #{
-      #  "frames" => {
-      #    0 => thumbnail_url("0"),
-      #    1 => thumbnail_url("1"),
-      #    2 => thumbnail_url("2"),
-      #    3 => thumbnail_url("3")
-      #  },
-      #  "quality" => {
-      #    "default" => thumbnail_url("default"),
-      #    "hqdefault" => thumbnail_url("hqdefault"),
-      #    "mqdefault" => thumbnail_url("mqdefault"),
-      #    "sddefault" => thumbnail_url("sddefault"),
-      #    "maxresdefault" => thumbnail_url("maxresdefault")
-      #  }
-      #}
     end
   end
 end
